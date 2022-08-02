770dd8f24f01eaf592ff655cfebf80f5f1c2444d
hjmoons <gywhd408@naver.com>
2022-08-02 10:58:39 +0900

    first commit

---
+++ src/main/java/com/demo/controller/SampleController.java
@@ -0,0 +1,12 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @RequestMapping("/sample")
+    public String sample() {
+        return "New Sample!!";
+    }
+}
