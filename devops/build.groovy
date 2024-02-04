bab8c064bee1f7c95316d6446a60a8b4d6f5f931
HyoJong <gywhd408@naver.com>
2024-02-04 21:56:40 +0900

    edit code

--- devops/build.groovy
+++ devops/build.groovy
@@ -10,7 +10,7 @@ pipeline {
         stage("BUILD") {
             steps {
                 //sh "mvn clean compile deploy -s settings.xml -Dmaven.test.skip=true -Dproject.version=${RELEASE_VERION}"
-                sh "mvn clean compile deploy -s settings.xml -Dmaven.test.skip=true -Dproject.version=0.0.3"
+                sh "mvn clean compile deploy -s settings.xml -Dmaven.test.skip=true -Dproject.version=0.0.6"
             }
         }
 
