18219f0882a1e1e8ac63f3a7528508954b810e99
hjmoons <gywhd408@naver.com>
2024-02-14 09:41:34 +0900

    migration spring 3 and set swagger

--- src/main/java/com/demo/user/UserRepository.java
+++ src/main/java/com/demo/user/UserRepository.java
@@ -3,8 +3,6 @@ package com.demo.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserRepository extends JpaRepository<UserVo, Long> {
     public UserVo findByLoginId(String loginId);
