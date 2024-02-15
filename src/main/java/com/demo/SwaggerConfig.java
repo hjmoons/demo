dfa1c5e54a371c3bd77364cee925d9e4c405504b
hjmoons <gywhd408@naver.com>
2024-02-15 17:16:41 +0900

    edit swagger config

--- src/main/java/com/demo/SwaggerConfig.java
+++ src/main/java/com/demo/SwaggerConfig.java
@@ -1,5 +1,26 @@
 package com.demo;
 
-public class SwaggerConfig {
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        info = @Info(title = "Demo App",
+                description = "Demo app api명세",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("DEMO API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
 }
